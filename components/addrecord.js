d94a026b34b5d994393e247c8fa605fc51c36231
anarchuluubayarsaikhan <anarchulu@gmail.com>
2024-08-30 17:57:39 +0800

    radio group

--- components/addrecord.js
+++ components/addrecord.js
@@ -1,5 +1,6 @@
 "use client"
-import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { Button } from "@/components/ui/button"
 import {
     Dialog,
@@ -26,6 +27,20 @@ export function  Addrecord () {
                     <div>
                         <Button variant="outline" classname ={`py-14 px-2 bg-primary-main-blue   ${expenseActive ==true}? ""`} onclick ={()=> setExpenseActive(true)}>Expense</Button>
                         <Button variant="outline" classname ={"py-14 px-2"}>Income</Button>
+                        <RadioGroup defaultValue="comfortable">
+                            <div className="flex items-center space-x-2">
+                            <RadioGroupItem value="default" id="r1" />
+                            <Label htmlFor="r1">Default</Label>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                            <RadioGroupItem value="comfortable" id="r2" />
+                            <Label htmlFor="r2">Comfortable</Label>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                            <RadioGroupItem value="compact" id="r3" />
+                            <Label htmlFor="r3">Compact</Label>
+                            </div>
+                        </RadioGroup>
                     </div>
                     <Button variant="outline" classname ={"py-14 px-2"}>Income</Button>
                 </DialogContent>
