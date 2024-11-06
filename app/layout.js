60b97159cbe50a17c71376652b6346dacef8109e
anarchuluubayarsaikhan <anarchulu@gmail.com>
2024-11-06 20:23:58 +0800

    second commit

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Suspense>
       <body className={inter.className}>{children}</body>
+      </Suspense>
     </html>
   );
 }
